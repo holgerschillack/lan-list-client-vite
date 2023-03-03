27469f07549e0e3b8178d888a8f23f34e40ec7bb
Holger Schillack <hs@jochenlinnemann.de>
2023-03-03 19:30:05 +0100

    Add toast in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import {createApp} from "vue";
 import App from "./App.vue";
-
+import VueToast from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-sugar.css";
 import "./main.css";
+
+Vue.use(VueToast);
 
 createApp(App).mount("#app");
